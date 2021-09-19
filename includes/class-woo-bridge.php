892e74ef5688a87458fd7af25482922c9e8cc908
hifny <ahmed.hifny@charisma-design.eu>
2021-09-19 23:42:32 +0200

    clear the cow payment

--- includes/class-woo-bridge.php
+++ includes/class-woo-bridge.php
@@ -26,7 +26,7 @@ class Woo_Bridge
         require_once(__DIR__ . "/abstract/abstract-woo-payment-gateway-cowpay.php");
         require_once(__DIR__ . "/payments/class-woo-cowpay-pos-fawry.php");
         require_once(__DIR__ . "/payments/class-woo-cowpay-credit-card.php");
-        require_once(__DIR__ . "/payments/class-woo-cowpay-checkout.php");
+        // require_once(__DIR__ . "/payments/class-woo-cowpay-checkout.php");
 
         require_once(__DIR__ . "/rest_api/class-woo-cowpay-api.php");
     }
@@ -43,7 +43,7 @@ class Woo_Bridge
         /** @see WC_Payment_Gateway_Cowpay_CC*/
         $methods[] = 'WC_Payment_Gateway_Cowpay_CC';
         /** @see WC_Payment_Gateway_Cowpay_Checkout*/
-        $methods[] = 'WC_Payment_Gateway_Cowpay_Checkout';
+        // $methods[] = 'WC_Payment_Gateway_Cowpay_Checkout';
         return $methods;
     }
 
